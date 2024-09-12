cc9f72ec6f5672c81ce84758cae4a6c50910065d
Avin19 <avinashy1993@gmail.com>
2024-09-12 13:39:10 +0530

    Worked on the GameManager

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("  UI Panels")]
+    [SerializeField] private Transform mainPanel;
+    [SerializeField] private Transform welcomePanel;
+    [SerializeField] private Transform levelPanel;
+
+    [Header("Buttons")]
+    [SerializeField] private Button startBtn;
+    [SerializeField] private Button quitBtn;
+
+
+    private void OnEnable()
     {
-        
+        startBtn.onClick.AddListener(() =>
+        {
+            SetAllThePanelFalse();
+            levelPanel.gameObject.SetActive(true);
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        welcomePanel.gameObject.SetActive(true);
     }
+
+    private void SetAllThePanelFalse()
+    {
+        welcomePanel.gameObject.SetActive(false);
+        mainPanel.gameObject.SetActive(false);
+        levelPanel.gameObject.SetActive(false);
+    }
+
+
+
+
 }
